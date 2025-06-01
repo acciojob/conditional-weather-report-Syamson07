24c390105e2cc21f495a3407a1a1b20823a9d76d
Syam Son Penumala <syamson132000@gmail.com>
2025-06-01 16:16:44 +0530

    Create WeatherDisplay.js

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function WeatherDisplay({ weather }) {
+  const color = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <p>
+        Temperature: <span style={{ color }}>{weather.temperature}</span>
+      </p>
+      <p>Conditions: {weather.conditions}</p>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
